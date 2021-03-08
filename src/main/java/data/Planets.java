fff7d219adf2424ca4fc00a4a1b47e98a9967f2e
suhachakka <suhasinichakka@gmail.com>
2021-03-07 23:02:54 -0600

    Finished bonus

--- src/main/java/data/Planets.java
+++ src/main/java/data/Planets.java
@@ -1,22 +1,27 @@
 package data;
 
+import java.math.BigInteger;
+
 public enum Planets {
-    MERCURY("Mercury", 88),
-    VENUS("Venus", 225),
-    EARTH("Earth", 365),
-    MARS("Mars", 687),
-    JUPITER("Jupiter", 4333),
-    SATURN("Saturn", 10759),
-    URANUS("Uranus", 30687),
-    NEPTUNE("Neptune", 602800),
-    PLUTO("Pluto", 90000);
+    MERCURY("Mercury", 88,5.427),
+    VENUS("Venus", 225,5.243),
+    EARTH("Earth", 365,5.513),
+    MARS("Mars", 687,3.93),
+    JUPITER("Jupiter", 4333,1.33),
+    SATURN("Saturn", 10759,0.687),
+    URANUS("Uranus", 30687,1.27),
+    NEPTUNE("Neptune", 602800,1.64),
+    PLUTO("Pluto", 90530,1.88);
 
     private final String displayName;
     private final long yearLength;
+    //bonus
+    private final double density;
 
-    Planets(String displayName, long yearLength){
+    Planets(String displayName, long yearLength,  double density){
         this.displayName=displayName;
-        this.yearLength = yearLength;
+        this.yearLength=yearLength;
+        this.density=density;
     }
 
     public String getDisplayName() {
@@ -27,11 +32,16 @@ public enum Planets {
         return yearLength;
     }
 
+    public double getDensity() {
+        return density;
+    }
+
     @Override
     public String toString() {
         return "Planets{" +
                 "displayName='" + displayName + '\'' +
                 ", yearLength=" + yearLength +
+                ", density=" + density +
                 '}';
     }
 }
